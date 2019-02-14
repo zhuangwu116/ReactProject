aff580f215c306d328193ec51c2bf6972d67d418
vca <zhuangwu@bjvca.com>
2019-02-14 15:56:43 +0800

    ?

--- restdjango/src/store/index.js
+++ restdjango/src/store/index.js
@@ -1,7 +1,7 @@
 import {createStore, applyMiddleware, compose} from "redux";
 import createSagaMiddleware from 'redux-saga';
 import reducer from './reducer';
-import mySagas from './sagas';
+import mySagas from '../sagas';
 
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
